c570597aaccedbfa80aecf398291d74ca7107b0d
Bruna Radeke <brunaradeke@hotmail.com>
2020-09-08 11:43:14 +0100

    Creating Button React styled-component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from './common';
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>App</h1>
+      <Button>Test</Button>
     </div>
   );
 }
