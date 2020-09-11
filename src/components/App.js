c912a213b9759a8447b76270cdf0ef0c13bdca54
Bruna Radeke <brunaradeke@hotmail.com>
2020-09-11 17:51:03 +0100

    6. Adding global styles using createGlobalStyle + adding Google fonts

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Button } from './common';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color:white;
+    min-height:100vh;
+    margin:0;
+    color:black;
+    font-family:'Kaushan Script'
+  }
+`;
 
 const App = () => {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <h1>App</h1>
       <Button>Test</Button>
-    </div>
+    </>
   );
 }
 
