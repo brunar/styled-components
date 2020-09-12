ab216b7fd3ddb46570dc081d05119785fd89d3dd
Bruna Radeke <brunaradeke@hotmail.com>
2020-09-12 12:18:09 +0100

    26. Interpolating functions to clean up style logic

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,5 +1,21 @@
 import styled, { css } from 'styled-components';
 
+const LargeStyles = ({ large }) => {
+    if (large) {
+        return css`
+            padding: 10px;
+            border-radius: 5px;
+            font-size: 1.5em;
+        `
+    } else {
+        return css`
+            padding: 8px;
+            border-radius: 4px;
+            font-size: 1em;
+        `
+    }
+}
+
 const Button = styled.button`
 color: white;
 background:${p => p.secondary ? p.theme.secondaryColor : p.theme.primaryColor};
@@ -9,16 +25,7 @@ border:none;
 width:100%;
 display:block;
 white-space:none;
-
-${p => p.large ? css`
-    padding: 10px;
-    border-radius: 5px;
-    font-size: 1.5em;
-` : css`
-    padding: 8px;
-    border-radius: 4px;
-    font-size: 1em;
-`}
+${LargeStyles};
 
     &:disabled {
         color: #666;
