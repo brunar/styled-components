6fdb4c9b74c74107b8355f2229afa46743666292
Bruna Radeke <brunaradeke@hotmail.com>
2020-09-12 12:11:52 +0100

    25. Referencing a styled component within another styled component

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -17,6 +17,12 @@ const Form = styled.form`
         text-align:center;
         margin: 10px 0;
     }
+    >${Button}:first-of-type {
+        margin-top:40px;
+    }
+    >${Input} {
+        margin-top:20px;
+    }
 `;
 
 let timeout;
@@ -56,6 +62,7 @@ export default function Login() {
             <Form onSubmit={handleSubmit}>
                 {loading ? <Spinner /> :
                     <>
+                        <span>Login if you have an account</span>
                         <Input
                             value={formFields.username}
                             onChange={handleInputChange}
